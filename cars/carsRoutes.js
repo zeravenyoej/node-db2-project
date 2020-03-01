e4d06b10cb96032b644334aeeeaea5f628fdb649
Joey Nevarez <josephnevarez12@gmail.com>
2020-02-29 18:58:20 -0800

    get and post routes working

--- cars/carsRoutes.js
+++ cars/carsRoutes.js
@@ -4,32 +4,39 @@ const db = require('../data/config')
 
 route.get("/", async (req, res, next) => {
     try {
-
-        
+        const cars = await db("cars")
+        res.json(cars)
     } catch(err) {
         next(err)
     }
 })
 
 
-// route.get("/:id", async (req, res, next) => {
-//     const { id } = req.params
-//     try {
+route.get("/:id", async (req, res, next) => {
+    const { id } = req.params
+    try {
+        //the FIRST returns a single object, as opposed to an array with one object
+        const car = await db("cars").where({id: id}).first()
+        res.json(car)
+    } catch(err) {
+        next(err)
+    }
+})
 
-        
-//     } catch(err) {
-//         next(err)
-//     }
-// })
+route.post("/", async (req, res, next) => {
+    try {
+        const { VIN, make, model, mileage } = req.body
+        if (!VIN || !make || !model || !mileage) {
+            return res.status(400).json({message: "Need VIN, make, model, and mileage please"})
+        }
 
-// route.get("/post", async (req, res, next) => {
-//     try {
-
-        
-//     } catch(err) {
-//         next(err)
-//     }
-// })
+        const [id] = await db("cars").insert(req.body)
+        const newCar = await db("cars").where({id: id}).first()
+        res.status(201).json(newCar)
+    } catch(err) {
+        next(err)
+    }
+})
 
 
 module.exports = route
